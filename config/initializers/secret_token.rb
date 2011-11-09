e0f62e140df24358f00e14981420885cf9664acf
Rafe Hatfield <rafe.hatfield@suite101.com>
2011-11-08 17:06:58 -0800

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileDepot::Application.config.secret_token = 'bed27b39e6138a7e9c7856d4997f8dbd074f681309c8423b165ad1a0835afaf29d35fa25c6e35f0ca8560c4b3f387b641fac7c513c1529d117e44410515a0793'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AgileDepot::Application.config.session_store :cookie_store, key: '_agile-depot_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AgileDepot::Application.config.session_store :active_record_store
